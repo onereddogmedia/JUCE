8b7e7fa53654dbd16efecdeec7d51c55eae23976
Peter Johnson <peter@onereddog.com.au>
2025-08-10 20:57:39 +1000

    [ORD] native title bar

--- modules/juce_audio_plugin_client/Standalone/juce_StandaloneFilterWindow.h
+++ modules/juce_audio_plugin_client/Standalone/juce_StandaloneFilterWindow.h
@@ -310,7 +310,11 @@ public:
         o.dialogTitle                   = TRANS ("Audio/MIDI Settings");
         o.dialogBackgroundColour        = o.content->getLookAndFeel().findColour (ResizableWindow::backgroundColourId);
         o.escapeKeyTriggersCloseButton  = true;
+#if JUCE_IOS
         o.useNativeTitleBar             = false;    // [ORD]: use native titlebar
+#else
+        o.useNativeTitleBar             = true;
+#endif
         o.resizable                     = false;
 
         o.launchAsync();
